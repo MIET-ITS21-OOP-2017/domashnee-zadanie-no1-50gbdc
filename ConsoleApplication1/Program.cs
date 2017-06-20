ba3c7c70862547b3253f693d09b406f544651b0e
kryksyh <kryksyh@gmail.com>
2017-06-20 08:50:16 +0300

    Hello world

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,7 +9,8 @@
     public class Program
     {
         public static void Main(string[] args) 
-        {        
+        {
+            System.Console.WriteLine("Hello, world");
         }
     }
 }
